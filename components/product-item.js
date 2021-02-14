03a455e5b964c88ac348b78ddc0a373d74897e5b
DESKTOP-UKSL5UM\Brendan <nxbt1998@gmail.com>
2021-02-13 23:35:15 -0800

    finished lab

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,134 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+
+    // Create a shadow root
+    this.attachShadow({mode: 'open'});
+
+    // Create wrapper for product attributes
+    const wrapper = document.createElement('li');
+    wrapper.setAttribute('class', 'product');
+
+    const style = document.createElement('style');
+    style.textContent = `
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+    `;
+
+    this.shadowRoot.append(style, wrapper);
+  }
+
+  connectedCallback() {
+    const wrapper = this.shadowRoot.querySelector('li');
+
+    const image = wrapper.appendChild(document.createElement('img'));
+    image.setAttribute('src', this.getAttribute('image'));
+    image.setAttribute('alt', this.getAttribute('title'));
+    image.setAttribute('width', '200');
+
+    const title = wrapper.appendChild(document.createElement('p'));
+    title.setAttribute('class', 'title');
+    title.textContent = this.getAttribute('title');
+
+    const price = wrapper.appendChild(document.createElement('p'));
+    price.setAttribute('class', 'price');
+    price.textContent = '$' + this.getAttribute('price');
+
+    const button = wrapper.appendChild(document.createElement('button'));
+    let addButton;
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    if(!cart[this.getAttribute('id')]) {
+      addButton = true;
+      button.textContent = 'Add to Cart';
+    }
+    else {
+      addButton = false;
+      button.textContent = 'Remove from Cart';
+      document.getElementById('cart-count').textContent++;
+    }
+    button.addEventListener('click', () => {
+      if(addButton) {
+        alert('Added to Cart!');
+        button.textContent = 'Remove from Cart';
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        cart[parseInt(this.getAttribute('id'), 10)] = true;
+        localStorage.setItem('cart', JSON.stringify(cart));
+        document.getElementById('cart-count').textContent++;
+        addButton = false;
+      }
+      else {
+        button.textContent = 'Add to Cart';
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        cart[parseInt(this.getAttribute('id'), 10)] = false;
+        localStorage.setItem('cart', JSON.stringify(cart));
+        document.getElementById('cart-count').textContent--;
+        addButton = true;
+      }
+    });
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,36 @@
-// Script.js
-
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  if(localStorage.getItem('jsonString') != null) {
+    generateProductListView(JSON.parse(localStorage.getItem('jsonString')));
+  }
+  else {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+      localStorage.setItem('jsonString', JSON.stringify(data));
+      generateProductListView(data)
+    });
+  }
 });
+
+function generateProductListView(productList) {
+  if(localStorage.getItem('cart') == null) {
+    let cart = [];
+    for(let i = 0; i < productList.length; i++) {
+      cart.push(false);
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+  
+  let productListElement = document.getElementById('product-list');
+
+  productList.forEach(product => {
+    let productElement = document.createElement('product-item');
+    productElement.setAttribute('id', product.id);
+    productElement.setAttribute('title', product.title);
+    productElement.setAttribute('image', product.image);
+    productElement.setAttribute('price', product.price);
+
+    productListElement.appendChild(productElement);
+  });
+}
